d0740abf01569151eb0c7bf62fc31fced247a594
AntonTrv <abtsochi@gmail.com>
2019-07-23 17:14:14 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+new hoverEffect({
+    parent: document.querySelector('.climate-img'),
+    intensity: 0.1,
+    image1: 'img/palms.jpeg',
+    image2: 'img/mountain.jpeg',
+    angle: Math.PI / 8,
+    speedIn: 1.2,
+    displacementImage: './img/blanket.jpg'
+});
+
+new hoverEffect({
+    parent: document.querySelector('.sea-img'),
+    intensity: 0.1,
+    image1: 'img/beach.jpeg',
+    image2: 'img/beach2.jpeg',
+    angle: Math.PI / 2,
+    speedIn: 1.2,
+    displacementImage: './img/blanket.jpg'
+});
+
+new hoverEffect({
+    parent: document.querySelector('.tracking-img'),
+    intensity: 0.1,
+    image1: 'img/tracking2.jpeg',
+    image2: 'img/river.jpeg',
+    angle: Math.PI / 8,
+    speedIn: 1.2,
+    displacementImage: './img/blanket.jpg'
+});
+
+new hoverEffect({
+    parent: document.querySelector('.entertainment-img'),
+    intensity: 0.1,
+    image1: 'img/paraglider.jpeg',
+    image2: 'img/bike.jpeg',
+    angle: Math.PI / 8,
+    speedIn: 1.2,
+    displacementImage: './img/blanket.jpg'
+});
